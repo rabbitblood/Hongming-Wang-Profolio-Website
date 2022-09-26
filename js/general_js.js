4ff9ae3a98e97ec8d1b408bfd6123969b9534e6b
rabbitblood <70198795+rabbitblood@users.noreply.github.com>
2022-09-25 18:58:01 -0700

    Add files via upload

--- js/general_js.js
+++ js/general_js.js
@@ -1,16 +1,36 @@
-let background = document.getElementById("background_animation_container");
+const background = document.getElementById("background_animation_container");
+const mainContent = document.getElementById("main");
+const title = document.getElementById("title");
 
-window.addEventListener("scroll", function (e) {
-    var scrollTop = window.scrollY;
-    var scrollHeight = document.body.offsetHeight;
-    var scrollButtom = window.outerHeight;
-    var scrollPercent = (scrollTop) / (scrollHeight - scrollButtom);
-    var scrollPercentRound = Math.round(scrollPercent * 100);
-    scrollChangeColor(scrollPercentRound);
+scrollEffect();
+
+window.addEventListener("scroll", function () {
+    scrollEffect();
 })
 
-function scrollChangeColor(scrollBarPercentage) {
-    background.style.backgroundColor = `rgba(${54 - scrollBarPercentage/2},
-                                             ${72 - scrollBarPercentage / 2},
-                                             ${107 - scrollBarPercentage / 2})`;
+function scrollEffect()
+{
+    var scrollTop = window.scrollY;
+    var scrollHeight = document.body.offsetHeight;
+    var scrollButtom = window.innerHeight;
+    var scrollPercent = (scrollTop) / (scrollHeight - scrollButtom);
+    var scrollPercentRound = Math.round(scrollPercent * 100);
+    scrollChangeBackgroundColor(scrollPercentRound);
+    scrollChangeMainContentOpacity(scrollPercentRound);
+    scrollChangeTitleOpacity(scrollPercentRound)
+}
+
+function scrollChangeBackgroundColor(scrollPercentRound) {
+    background.style.backgroundColor = `rgba(${54 - scrollPercentRound/2},
+                                             ${72 - scrollPercentRound / 2},
+                                             ${107 - scrollPercentRound / 2})`;
+}
+
+function scrollChangeMainContentOpacity(scrollPercentRound)
+{
+    mainContent.style.opacity = scrollPercentRound / 100 /1.5;
+}
+
+function scrollChangeTitleOpacity(scrollPercentRound) {
+    title.style.opacity = 1 - scrollPercentRound / 100*2;
 }
