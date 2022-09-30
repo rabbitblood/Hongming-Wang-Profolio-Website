226e6b8419a298bd1ac32f29273a7eb0047454e4
rabbitblood <70198795+rabbitblood@users.noreply.github.com>
2022-09-29 17:18:29 -0700

    Add files via upload

--- js/general_js.js
+++ js/general_js.js
@@ -1,36 +1,75 @@
+//-------------------------------------for scroll effects---------------------------------------
 const background = document.getElementById("background_animation_container");
 const mainContent = document.getElementById("main");
 const title = document.getElementById("title");
 
-scrollEffect();
+let viewportHeight = window.innerHeight;
+let viewportWidth = window.innerWidth;
+let currentScroll = 0;
+let bodyHeight = document.documentElement.scrollHeight - viewportHeight;
 
 window.addEventListener("scroll", function () {
-    scrollEffect();
+    viewportHeight = window.innerHeight;
+    viewportWidth = window.innerWidth;
+    bodyHeight = document.documentElement.scrollHeight - viewportHeight;
+
+    currentScroll = this.scrollY;
+    console.log(this.scroll.innerHeight);
+
+    scrollChangeBackgroundColor();
+    scrollChangeMainContentOpacity();
+    scrollChangeTitleOpacity();
 })
 
-function scrollEffect()
-{
-    var scrollTop = window.scrollY;
-    var scrollHeight = document.body.offsetHeight;
-    var scrollButtom = window.innerHeight;
-    var scrollPercent = (scrollTop) / (scrollHeight - scrollButtom);
-    var scrollPercentRound = Math.round(scrollPercent * 100);
-    scrollChangeBackgroundColor(scrollPercentRound);
-    scrollChangeMainContentOpacity(scrollPercentRound);
-    scrollChangeTitleOpacity(scrollPercentRound)
-}
 
-function scrollChangeBackgroundColor(scrollPercentRound) {
+function scrollChangeBackgroundColor() {
+    var scrollPercent = currentScroll / bodyHeight;
+    console.log(bodyHeight +", "+currentScroll);
+    var scrollPercentRound = Math.round(scrollPercent * 100);
+
     background.style.backgroundColor = `rgba(${54 - scrollPercentRound/2},
                                              ${72 - scrollPercentRound / 2},
                                              ${107 - scrollPercentRound / 2})`;
 }
 
-function scrollChangeMainContentOpacity(scrollPercentRound)
-{
-    mainContent.style.opacity = scrollPercentRound / 100 /1.5;
+function scrollChangeMainContentOpacity(){
+    var finalOpacityRatio = 0.9;
+    var calculatedScrollValue = 0;
+    if(currentScroll > viewportHeight){
+        calculatedScrollValue = viewportHeight;
+    }
+    else{
+        calculatedScrollValue = currentScroll;
+    }
+    var opacityPercent = (calculatedScrollValue/viewportHeight) * finalOpacityRatio;
+    mainContent.style.opacity = opacityPercent;
 }
 
-function scrollChangeTitleOpacity(scrollPercentRound) {
-    title.style.opacity = 1 - scrollPercentRound / 100*2;
+function scrollChangeTitleOpacity() {
+    var calculatedScrollValue = 0;
+    if(currentScroll > viewportHeight){
+        calculatedScrollValue = viewportHeight;
+    }
+    else{
+        calculatedScrollValue = currentScroll;
+    }
+    var opacityPercent = viewportHeight/calculatedScrollValue/2 -1;
+    title.style.opacity = opacityPercent;
+}
+
+//-------------------------------------for menu button effect---------------------------------------
+const navBar = document.getElementById("navBar");
+navBar.style.animationName = "none";
+
+let menuExpanded = false;
+
+function menuButtonClick(){
+    if(menuExpanded){
+        navBar.style.animationName = "menu_shrink";
+        menuExpanded = false;
+    }
+    else{
+        navBar.style.animationName = "menu_expand";
+        menuExpanded = true;
+    }
 }
